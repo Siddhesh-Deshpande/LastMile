c2a3367c13776a483fda0145df3ccf9980006301
Abhinav Kumar <Abhinav.Kumar@iiitb.ac.in>
2025-11-26 22:11:20 +0530

    updated scripts and ansible

--- scripts/notification_stream.py
+++ scripts/notification_stream.py
@@ -1,14 +1,17 @@
 import asyncio
 from aiohttp_sse_client import client as sse_client
 
-async def listen_sse(url, jwt, role):
+async def listen_sse(url : str, jwt : str, role : str):
     headers = {"Authorization": f"Bearer {jwt}"}
     async with sse_client.EventSource(url, headers=headers) as event_source:
         async for event in event_source:
             print(f"[{role}] : {event.data}")
 
 def main():
-    url = "http://localhost:8086/notifications/stream"
+    minkube_ip = input("enter minikube ip : ")
+    notification_port = int(input("enter port of notification service : "))
+    
+    url = f"http://{minkube_ip}:{notification_port}/notifications/stream"
     role = input("Enter the role: ")
     jwt = input("Enter the Jwt Token: ")
     asyncio.run(listen_sse(url, jwt, role))
--- scripts/testing_script.py
+++ scripts/testing_script.py
@@ -2,45 +2,37 @@ import requests
 import datetime
 import time
 
-USERSERVICE = "http://localhost:8081"
-DRIVERSERVICE = "http://localhost:8082"
-RIDERSERVICE = "http://localhost:8083"
-TRIPSERVICE = "http://localhost:8084"
-# register a user
-def register_a_rider_and_driver(rider_name,rider_pass,driver_name,driver_pass):
-    url = USERSERVICE + "/users/register"
+def register(user_name : str,user_pass : str,roles : list[str],url : str):
     params = {
-        "username" : rider_name,
-        "password" : rider_pass,
-        "roles" : ["rider"]
+        "username" : user_name,
+        "password" : user_pass,
+        "roles" : roles
     }
     response = requests.post(url=url,json=params)
-    print(response.text)
-    params["username"] = driver_name
-    params["password"] = driver_pass
-    params["roles"] = ["driver"]
-    response = requests.post(url=url,json=params)
-    print(response.text)
+    print(f"user {user_name} : {response.text}")
 
-def login(rider_name,rider_pass,driver_name,driver_pass):
-    url = USERSERVICE + "/users/login"
+def register_both(rider_name : str,rider_pass : str,driver_name : str,driver_pass : str,url : str):
+    register(rider_name,rider_pass,["rider"],url)
+    register(driver_name,driver_pass,["driver"],url)
+
+def login(user_name : str,user_pass: str,role : str,url : str) -> str:
     params = {
-        "username" : rider_name,
-        "password" : rider_pass,
-        "role" : "rider"
+        "username" : user_name,
+        "password" : user_pass,
+        "role" : role
     }
     response = requests.post(url=url,json=params)
-    rider_jwt = response.json().get("token")
-    params["username"] = driver_name
-    params["password"] = driver_pass
-    params["role"] = "driver"
-    response = requests.post(url=url,json=params)
-    driver_jwt = response.json().get("token")
-    print(f"rider jwt : {rider_jwt}\ndriver jwt : {driver_jwt}")
+    return response.json().get("token")
+
+def login_both(rider_name : str,rider_pass : str,driver_name : str,driver_pass : str,url : str) -> tuple[str,str]:
+    rider_jwt = login(rider_name,rider_pass,"rider",url)
+    driver_jwt = login(driver_name,driver_pass,"driver",url)
+    print(f"{rider_jwt=}")
+    print(f"{driver_jwt=}")
     return (rider_jwt,driver_jwt)
+    
 
-def register_route(jwt_token,starting,dest,seats):
-    url = DRIVERSERVICE + "/api/register-route"
+def register_route(jwt_token : str,starting : str,dest,seats : int,url : str) -> int:
     headers = {
         "Authorization": f"Bearer {jwt_token}",
         "Content-Type": "application/json"
@@ -56,8 +48,7 @@ def register_route(jwt_token,starting,dest,seats):
     return routeId
 
 
-def register_arrival(jwt,dest,station):
-    url = RIDERSERVICE + "/api/register-arrival"
+def register_arrival(jwt : str,dest : str,station : str,url : str):
     headers = {
         "Authorization": f"Bearer {jwt}",
         # "Content-Type": "application/json"
@@ -71,8 +62,7 @@ def register_arrival(jwt,dest,station):
     data = requests.post(url=url,headers=headers,json=params)
     print(data.text)
 
-def update_loc(jwt,routeid,loc):
-    url = DRIVERSERVICE + "/api/update-location"
+def update_loc(jwt : str,routeid : int,loc : str,url : str):
     headers = {
         "Authorization": f"Bearer {jwt}",
         "Content-Type": "application/json"
@@ -84,31 +74,74 @@ def update_loc(jwt,routeid,loc):
     data = requests.patch(url=url,headers=headers,json=params)
     print(data.text)
 
-def confirm_ride(jwt,tripid,arrivalid):
-    url = TRIPSERVICE+"/api/confirmTrip"
+def confirm_ride(jwt : str,tripid : int,arrivalid : int,url : str):
     header={
         "Authorization":f"Bearer {jwt}",
         "Content-Type": "application/json"
     }
     param = {
         "tripId" : tripid,
-        "arrivalId" :arrivalid
+        "arrivalId" : arrivalid
     }
     response = requests.post(url,json=param,headers=header)
     print(response.text)
 
-dest = "marathalli"
+def main():
+    rider_name = "abhinav"
+    rider_pass = "abhinav"
+    driver_name = "siddhesh"
+    driver_pass = "siddhesh"
+    destination = "marathalli"
+    station = "METROSTATION_SILKBOARD"
+    #inputs
+    minikube_ip = input("enter ip of minikube : ")
+    user_port = int(input("enter port of user-service : "))
+    rider_port = int(input("enter port of rider-service : "))
+    driver_port = int(input("enter port of driver-service : "))
+    trip_port = int(input("enter port of trip-service : "))
+    #hosts
+    user_service_host = f"http://{minikube_ip}:{user_port}"
+    rider_service_host = f"http://{minikube_ip}:{rider_port}"
+    driver_service_host = f"http://{minikube_ip}:{driver_port}"
+    trip_service_host = f"http://{minikube_ip}:{trip_port}"
+    #urls
+    url_register = user_service_host + "/users/register"
+    url_login = user_service_host + "/users/login"
+    route_url = driver_service_host + "/api/register-route"
+    url_arrival = rider_service_host + "/api/register-arrival"
+    update_loc_url = driver_service_host + "/api/update-location"
+    conf_trip_url = trip_service_host + "/api/confirmTrip"
 
-register_a_rider_and_driver("abhi","abhi","sid","sid")
-tokens = login("abhi","abhi","sid","sid")
-route_id = register_route(tokens[1],"electronic_city",dest,4)
-register_arrival(tokens[0],dest,"METROSTATION_SILKBOARD")
-time.sleep(30)
-update_loc(tokens[1],route_id,"hsrlayout")
-time.sleep(30)
-update_loc(tokens[1],route_id,"METROSTATION_SILKBOARD")
-tripId=int(input("Enter TripId: "))
-arrivalId=int(input("Enter ArrivalId: "))
-confirm_ride(tokens[0],tripId,arrivalId)
-time.sleep(30)
-update_loc(tokens[1],route_id,dest)
+    register_both(rider_name,rider_pass,driver_name,driver_pass,url_register)
+    rider_jwt, driver_jwt = login_both(rider_name,rider_pass,driver_name,driver_pass,url_login)
+    route_id = register_route(driver_jwt,"electropnic_city",destination,4,route_url)
+    register_arrival(rider_jwt,destination,station,url_arrival)
+    time.sleep(30)
+    update_loc(driver_jwt,route_id,"hsrlayout",update_loc_url)
+    time.sleep(30)
+    update_loc(driver_jwt,route_id,station,update_loc_url)
+    tripId=int(input("Enter TripId : "))
+    arrivalId=int(input("Enter ArrivalId : "))
+    confirm_ride(rider_jwt,tripId,arrivalId,conf_trip_url)
+    time.sleep(30)
+    update_loc(driver_jwt,route_id,destination,update_loc_url)
+
+
+if __name__=="__main__":
+    main()
+
+# dest = "marathalli"
+
+# register_a_rider_and_driver("abhi","abhi","sid","sid")
+# tokens = login("abhi","abhi","sid","sid")
+# route_id = register_route(tokens[1],"electronic_city",dest,4)
+# register_arrival(tokens[0],dest,"METROSTATION_SILKBOARD")
+# time.sleep(30)
+# update_loc(tokens[1],route_id,"hsrlayout")
+# time.sleep(30)
+# update_loc(tokens[1],route_id,"METROSTATION_SILKBOARD")
+# tripId=int(input("Enter TripId: "))
+# arrivalId=int(input("Enter ArrivalId: "))
+# confirm_ride(tokens[0],tripId,arrivalId)
+# time.sleep(30)
+# update_loc(tokens[1],route_id,dest)
